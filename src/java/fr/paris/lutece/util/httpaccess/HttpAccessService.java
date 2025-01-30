5f59b928265149a9be1f786991c40d09821d62f4
Rémi Zara <remi.zara@paris.fr>
2025-01-30 15:12:05 +0100

    LUT-29302: Move default HttpClientConfiguration in its own class
    So that subclasser of HttpAccessService can reuse this configuration

--- src/java/fr/paris/lutece/util/httpaccess/HttpAccessService.java
+++ src/java/fr/paris/lutece/util/httpaccess/HttpAccessService.java
@@ -44,10 +44,6 @@ import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.core5.http.HttpHost;
 import org.apache.hc.core5.util.Timeout;
 
-import fr.paris.lutece.portal.service.util.AppLogService;
-import fr.paris.lutece.portal.service.util.AppPropertiesService;
-
-
 /**
  * HttpAccessService.
  */
@@ -55,60 +51,8 @@ public class HttpAccessService implements ResponseStatusValidator
 {
     /** The Constant DEFAULT_RESPONSE_CODE_AUTHORIZED. */
     private static final String DEFAULT_RESPONSE_CODE_AUTHORIZED = "200,201,202";
-
-    /** The Constant PROPERTY_PROXY_HOST. */
-    private static final String PROPERTY_PROXY_HOST = "httpAccess.proxyHost";
-
-    /** The Constant PROPERTY_PROXY_PORT. */
-    private static final String PROPERTY_PROXY_PORT = "httpAccess.proxyPort";
-
-    /** The Constant PROPERTY_PROXY_USERNAME. */
-    private static final String PROPERTY_PROXY_USERNAME = "httpAccess.proxyUserName";
-
-    /** The Constant PROPERTY_PROXY_PASSWORD. */
-    private static final String PROPERTY_PROXY_PASSWORD = "httpAccess.proxyPassword";
-
-    /** The Constant PROPERTY_HOST_NAME. */
-    private static final String PROPERTY_HOST_NAME = "httpAccess.hostName";
-
-    /** The Constant PROPERTY_DOMAIN_NAME. */
-    private static final String PROPERTY_DOMAIN_NAME = "httpAccess.domainName";
-
-    /** The Constant PROPERTY_REALM. */
-    private static final String PROPERTY_REALM = "httpAccess.realm";
-
-    /** The Constant PROPERTY_NO_PROXY_FOR. */
-    private static final String PROPERTY_NO_PROXY_FOR = "httpAccess.noProxyFor";
-
-    /** The Constant PROPERTY_CONTENT_CHARSET. */
-    private static final String PROPERTY_CONTENT_CHARSET = "httpAccess.contentCharset";
-
-    /** The Constant PROPERTY_ELEMENT_CHARSET. */
-    private static final String PROPERTY_ELEMENT_CHARSET = "httpAccess.elementCharset";
-
-    /** The Constant PROPERTY_SOCKET_TIMEOUT. */
-    private static final String PROPERTY_SOCKET_TIMEOUT = "httpAccess.socketTimeout";
-
-    /** The Constant PROPERTY_CONNECTION_TIMEOUT. */
-    private static final String PROPERTY_CONNECTION_TIMEOUT = "httpAccess.connectionTimeout";
-
-    /** The Constant PROPERTY_CONNECTION_POOL_ENABLED. */
-    private static final String PROPERTY_CONNECTION_POOL_ENABLED = "httpAccess.connectionPoolEnabled";
-
-    /** The Constant PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION. */
-    private static final String PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION = "httpAccess.connectionPoolMaxTotalConnections";
-
-    /** The Constant PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION_PER_HOST. */
-    private static final String PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION_PER_HOST = "httpAccess.connectionPoolMaxConnectionsPerHost";
-
     /** The Constant PROPERTY_HTTP_RESPONSES_CODE_AUTHORIZED. */
     private static final String PROPERTY_HTTP_RESPONSES_CODE_AUTHORIZED = "httpAccess.responsesCodeAuthorized";
-
-    /** The Constant PROPERTY_HTTP_PROTOCOLE_CONTENT_CHARSET. */
-    private static final String PROPERTY_HTTP_PROTOCOLE_CONTENT_CHARSET = "http.protocol.content-charset";
-
-    /** The Constant PROPERTY_HTTP_PROTOCOLE_ELEMENT_CHARSET. */
-    private static final String PROPERTY_HTTP_PROTOCOLE_ELEMENT_CHARSET = "http.protocol.element-charset";
 
     /** The _singleton. */
     private static HttpAccessService _singleton;
@@ -141,10 +85,7 @@ public class HttpAccessService implements ResponseStatusValidator
     {
         if ( _singleton == null )
         {
-            _singleton = new HttpAccessService( );
-            // WARNING if .init( ) throw exception, the singleton is already defined
-            _singleton.init( );
-            _singleton._responseValidator = SimpleResponseValidator.loadFromProperty( PROPERTY_HTTP_RESPONSES_CODE_AUTHORIZED, DEFAULT_RESPONSE_CODE_AUTHORIZED );
+            _singleton = new HttpAccessService( new PropertiesHttpClientConfiguration( ) );
         }
 
         return _singleton;
@@ -157,12 +98,7 @@ public class HttpAccessService implements ResponseStatusValidator
     {
     	super();
     	_httpClientConfiguration=httpClienConfiguration;
-    	
-    }
-    
-    private HttpAccessService( )
-    {
-    	
+    	_responseValidator = SimpleResponseValidator.loadFromProperty( PROPERTY_HTTP_RESPONSES_CODE_AUTHORIZED, DEFAULT_RESPONSE_CODE_AUTHORIZED );
     }
 
     /**
@@ -343,65 +279,6 @@ public class HttpAccessService implements ResponseStatusValidator
 
         return states [nLength];
     }
-
-    /**
-     * init properties.
-     */
-    private void init( )
-    {
-    	_httpClientConfiguration=new HttpClientConfiguration();
-        _httpClientConfiguration.setProxyHost(AppPropertiesService.getProperty( PROPERTY_PROXY_HOST ));
-        _httpClientConfiguration.setProxyPort (AppPropertiesService.getProperty( PROPERTY_PROXY_PORT ));
-        _httpClientConfiguration.setProxyUserName (AppPropertiesService.getProperty( PROPERTY_PROXY_USERNAME ));
-        _httpClientConfiguration.setProxyPassword( AppPropertiesService.getProperty( PROPERTY_PROXY_PASSWORD ));
-        _httpClientConfiguration.setHostName (AppPropertiesService.getProperty( PROPERTY_HOST_NAME ));
-        _httpClientConfiguration.setDomainName ( AppPropertiesService.getProperty( PROPERTY_DOMAIN_NAME ));
-        _httpClientConfiguration.setRealm( AppPropertiesService.getProperty( PROPERTY_REALM ));
-        _httpClientConfiguration.setNoProxyFor (AppPropertiesService.getProperty( PROPERTY_NO_PROXY_FOR ));
-        _httpClientConfiguration.setContentCharset (AppPropertiesService.getProperty( PROPERTY_CONTENT_CHARSET ));
-        _httpClientConfiguration.setElementCharset ( AppPropertiesService.getProperty( PROPERTY_ELEMENT_CHARSET ));
-        _httpClientConfiguration.setConnectionPoolEnabled ( AppPropertiesService.getPropertyBoolean( PROPERTY_CONNECTION_POOL_ENABLED, false ));
-        
-		try {
-			_httpClientConfiguration.setSocketTimeout( StringUtils.isNotEmpty( AppPropertiesService.getProperty(PROPERTY_SOCKET_TIMEOUT) ) 	? Integer.parseInt(AppPropertiesService.getProperty(PROPERTY_SOCKET_TIMEOUT))
-					: null);
-
-		} catch (NumberFormatException e) {
-			AppLogService.error("Error during initialisation of socket timeout ", e);
-		}
-
-		try {
-			_httpClientConfiguration
-					.setConnectionTimeout( StringUtils.isNotEmpty(  AppPropertiesService.getProperty(PROPERTY_CONNECTION_TIMEOUT) )
-							? Integer.parseInt(AppPropertiesService.getProperty(PROPERTY_CONNECTION_TIMEOUT))
-							: null);
-
-		} catch (NumberFormatException e) {
-			AppLogService.error("Error during initialisation of connection timeout ", e);
-		}
-		try {
-			_httpClientConfiguration.setConnectionPoolMaxTotalConnection(
-					StringUtils.isNotEmpty( AppPropertiesService.getProperty(PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION) )
-							? Integer.parseInt(
-									AppPropertiesService.getProperty(PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION))
-							: null);
-
-		} catch (NumberFormatException e) {
-			AppLogService.error("Error during initialisation of Connection Pool Maxt Total Connection ", e);
-		}
-		try {
-			_httpClientConfiguration.setConnectionPoolMaxConnectionPerHost(
-					StringUtils.isNotEmpty(  AppPropertiesService.getProperty(PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION_PER_HOST) ) 
-							? Integer.parseInt(AppPropertiesService
-									.getProperty(PROPERTY_CONNECTION_POOL_MAX_TOTAL_CONNECTION_PER_HOST))
-							: null);
-
-		} catch (NumberFormatException e) {
-			AppLogService.error("Error during initialisation of Connection Pool Maxt Total Connection Per Host ", e);
-		}
-       
-        
-        }
 
     /**
      * Default Response status Validation
